70d1e7dd5c5f10281d59be34b2e7e3a69688ab0c
Chalapathi Addanki <chalapathi.addanki@nB.local>
2018-05-04 18:45:29 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { DocumentViewModule } from 'ngx-document-view';
 import { AppComponent } from './app.component';
+import { ImageViewerModule } from 'ng2-image-viewer';
+import { Page1Component } from './page1/page1.component';
+import { PdfmakeModule } from 'ng-pdf-make';
+import { PdfmakeService } from 'ng-pdf-make/pdfmake/pdfmake.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DocumentViewModule,
+    ImageViewerModule,
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [PdfmakeService],
+  bootstrap: [Page1Component]
 })
 export class AppModule { }
